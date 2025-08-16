884fadb4b2e4af89346c2db5ceb8abf0359de4e6
AleEMP <aleja.emp11@gmail.com>
2025-08-15 23:16:27 -0500

    Clase de OOP

---
+++ Clase 1/src/Practicas/OOPMain.java
@@ -0,0 +1,14 @@
+package Practicas;
+
+import Practicas.models.Transactions;
+
+import java.util.Scanner;
+
+public class OOPMain {
+    public static void main(String[] args) {
+        Scanner scaner=new Scanner(System.in);
+        Transactions trans1= new Transactions();
+        trans1.setNombre("TranssaciónCodigo");
+
+    }
+}
